ac31951d2b7baae60c501012788a61078c4c1b91
Exc0mmun1cad0 <fauxmen4@gmail.com>
2025-03-29 19:05:05 +0300

    add create vote method

---
+++ internal/repo/tarantool/create_vote.go
@@ -0,0 +1,43 @@
+package tarantool
+
+import (
+	"fmt"
+	"vote-bot/internal/entity"
+
+	"github.com/tarantool/go-tarantool/v2"
+)
+
+const (
+	createVoteFunc = "create_vote"
+)
+
+// CreateVote adds new vote record to space "votes".
+// It uses lua-defined create_vote() func under the hood.
+//
+// In case user votes 2nd time, his vote will simply be updated.
+func (r *Repo) CreateVote(vote entity.Vote) (*entity.Vote, error) {
+	const op = "tarantool.VoteRepo.CreateVote"
+
+	data, err := r.conn.Do(
+		tarantool.NewCall17Request(createVoteFunc).
+			Args([]any{vote.User, vote.PollID, vote.OptionIDs}),
+	).Get()
+	if err != nil {
+		return nil, fmt.Errorf("%s: failed to create vote: %w", op, err)
+	}
+
+	tuple := data[0].([]interface{})
+	tupleIDs := tuple[3].([]interface{})
+	optionIDs := make([]uint64, len(tupleIDs))
+	for i := range optionIDs {
+		optionIDs[i] = uint64(tupleIDs[i].(int8))
+	}
+	newVote := entity.Vote{
+		VoteID:    uint64(tuple[0].(int8)),
+		User:      tuple[1].(string),
+		PollID:    uint64(tuple[2].(int8)),
+		OptionIDs: optionIDs,
+	}
+
+	return &newVote, nil
+}
