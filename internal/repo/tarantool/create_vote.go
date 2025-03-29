b3523f3b237c756f99bcce11967e5cf0e2f5cf63
Exc0mmun1cad0 <fauxmen4@gmail.com>
2025-03-29 22:21:53 +0300

    refactor CreateVote func

--- internal/repo/tarantool/create_vote.go
+++ internal/repo/tarantool/create_vote.go
@@ -14,7 +14,7 @@ const (
 // CreateVote adds new vote record to space "votes".
 // It uses lua-defined create_vote() func under the hood.
 //
-// In case user votes 2nd time, his vote will simply be updated.
+// In case user votes second time, his vote will simply be updated.
 func (r *Repo) CreateVote(vote entity.Vote) (*entity.Vote, error) {
 	const op = "tarantool.VoteRepo.CreateVote"
 
@@ -26,8 +26,8 @@ func (r *Repo) CreateVote(vote entity.Vote) (*entity.Vote, error) {
 		return nil, fmt.Errorf("%s: failed to create vote: %w", op, err)
 	}
 
-	tuple := data[0].([]interface{})
-	tupleIDs := tuple[3].([]interface{})
+	tuple := data[0].([]any)
+	tupleIDs := tuple[3].([]any)
 	optionIDs := make([]uint64, len(tupleIDs))
 	for i := range optionIDs {
 		optionIDs[i] = uint64(tupleIDs[i].(int8))
