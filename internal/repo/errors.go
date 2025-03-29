47b923bb3ce42e896cd3d3530b7bb82da09761ff
Exc0mmun1cad0 <fauxmen4@gmail.com>
2025-03-30 02:02:33 +0300

    add error for case no options found

--- internal/repo/errors.go
+++ internal/repo/errors.go
@@ -5,4 +5,5 @@ import "errors"
 var (
 	ErrPollDoesNotExist = errors.New("poll with this id does not exist")
 	ErrNoVotes          = errors.New("no votes found for poll with this id")
+	ErrNoOptionsFound   = errors.New("no options for the poll was found")
 )
